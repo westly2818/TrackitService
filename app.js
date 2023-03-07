59f5e1a5b40d806d90e016a09a7ee6ddf1e4fe78
westly2818 <westly2818@gmail.com>
2023-03-08 02:16:45 +0530

    push

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ app.use(bodyParser.json({limit: '1mb'}));
 app.use(bodyParser.urlencoded({limit: '1mb',extended: false}));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-let root = __dirname + '/dist';
-app.use(express.static(root));
+
 
 app.use("/api/dashboard", dashboardRoutes);
 
